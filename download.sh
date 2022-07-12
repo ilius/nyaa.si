ebf4d3108e888a67fd5e939ad88cf15815af5fb7
Saeed Rasooli <saeed.gnu@gmail.com>
2022-07-12 08:30:54 +0430

    update download.sh

--- download.sh
+++ download.sh
@@ -1,1 +1,11 @@
-wget -O top.html https://nyaa.si/?s=seeders&o=desc
+URL='https://nyaa.si/?s=seeders&o=desc'
+DTIME=$(date +%Y-%m-%d-%H%M%S)
+
+if ! wget -O "top-$DTIME.html" "$URL" ; then
+	rm "top-$DTIME.html"
+	exit 1
+fi
+
+ln -sf "top-$DTIME.html" top.html
+
+./filter-json-pretty.sh > top.json
