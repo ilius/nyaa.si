099a801dd47bba971a8a0dfafbb72e68d46604d8
Saeed Rasooli <saeed.gnu@gmail.com>
2022-08-29 08:01:58 +0430

    add filter-json-compact-full.sh

---
+++ filter-json-compact-full.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "["
+last_line=
+
+# with: some_command | while read -r line ; do ...
+# a sub-shell is created to run the while loop,
+# so the last_line variable is isolated in that sub-shell
+# and becomes inaccessible outside the loop
+
+while read -r line ; do
+	if [ -z "$line" ] ; then
+		continue
+	fi
+	if [ -n "$last_line" ] ; then
+		echo "$last_line,"
+	fi
+	last_line=$line
+done < <(./filter.py --json "$@")
+
+echo "$last_line"
+echo "]"
+
